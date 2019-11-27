74ba18e48f2a28d2ebef2097f322a1ca7ae6424c
Dominik Nitschmann <dominik.nitschmann@googlemail.com>
2019-11-27 15:40:17 +0100

    Outsourced some constants to config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-AVAILABLE_EMBEDDINGS = ["cifar10", "cifar100", "imagenet"]
+AVAILABLE_EMBEDDINGS = ["imagenet"]
 VGG16_WEIGHTS_IMAGE_NET_PATH = "pretrained_vgg_weights/vgg16_weights_imagenet.h5"
 VGG16_WEIGHTS_CIFAR_10_PATH = "pretrained_vgg_weights/vgg16_weights_cifar10.h5"
 VGG16_WEIGHTS_CIFAR_100_PATH = "pretrained_vgg_weights/vgg16_weights_cifar100.h5"
@@ -9,3 +9,8 @@ INPUT_WIDTH = 224
 INPUT_HEIGHT = 224
 N_CLASSES = 22
 IMAGE_ORDERING = "channels_last"
+
+SRC_DIR_TRAINING_X = "VOCdevkit Training/VOC2012/JPEGImages/"
+FILE_TYPE_TRAINING_X = ".jpg"
+SRC_DIR_TRAINING_Y = "VOCdevkit Training/VOC2012/SegmentationClass/"
+FILE_TYPE_TRAINING_Y = ".png"
